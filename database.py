0168c05fe0ea1670ead5419358cdabe0b4f86e53
Gabriel Padilla <padillahernangabriel@gmail.com>
2025-06-09 15:15:08 -0300

    Configurar get_connection para usar DATABASE_URL

--- database.py
+++ database.py
@@ -9,14 +9,11 @@ from typing import Optional, Dict
 logger = logging.getLogger("uvicorn.error")
 
 
+import os
+import psycopg2
+
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=int(os.getenv("DB_PORT")),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        dbname=os.getenv("DB_NAME")
-    )
+    return psycopg2.connect(os.getenv("DATABASE_URL"))
 
 
 def init_db():
