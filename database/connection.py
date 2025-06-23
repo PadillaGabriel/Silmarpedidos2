7af675c28e122fef8bbb8e40240cf9aae3396d7f
PadillaGabriel <padillahernangabriel@gmail.com>
2025-06-23 16:32:57 -0300

    Update connection.py

--- database/connection.py
+++ database/connection.py
@@ -4,6 +4,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Aplica connect_args solo si es SQLite
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Session = sessionmaker(bind=engine)
